e6704f36f7f7c88bd5055745bfdfec6f0f77e19a
Eduardo Reche Lopes <eduardo.rechelopes@exxonmobil.com>
2021-09-13 18:58:47 -0300

    feat: animate card

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react';
 import { useWindowDimensions, ViewProps } from 'react-native';
 import {
+  Easing,
+  Extrapolate,
+  interpolate,
+  runOnJS,
   useAnimatedStyle,
   useSharedValue,
+  withSpring,
   withTiming
 } from 'react-native-reanimated';
 
@@ -14,21 +19,28 @@ interface CardAnimationProps extends ViewProps {
 
 export function CardAnimation({ children, ...rest }: CardAnimationProps) {
   const { width: displayWidth } = useWindowDimensions();
+
   const cardOpacity = useSharedValue(0);
   const cardOffset = useSharedValue(0.25 * displayWidth);
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      // TODO - setup animated style
+      opacity: interpolate(cardOffset.value, [0, 25, 30, 50], [1, 0.8, 0.5, 0], Extrapolate.CLAMP),
+      transform: [
+        {translateX: interpolate(
+          cardOffset.value,
+          [0, 50],
+          [0, 0.25 * displayWidth],
+          Extrapolate.CLAMP
+        )}
+      ]
     }
   })
 
   useEffect(() => {
-    /**
-     * TODO - setup cardOpacity.value and cardOffset.value with
-     * withTiming()
-     */
-  }, []);
+    cardOffset.value = withTiming(0, {duration: 1000});
+    cardOpacity.value = withTiming(1, {duration: 1000});
+  }, [cardOpacity.value, cardOffset.value]);
 
   return (
     <AnimationContainer {...rest} style={animatedStyle}>
